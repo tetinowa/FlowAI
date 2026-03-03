8541c3d2088dfa9c80915ee1721f784d05314e23
ssol20071127-eng <ssol20071127@gmail.com>
2026-03-03 14:52:17 +0800

    landing page end

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,28 +4,19 @@
 import Hero from "@/app/_components/Hero"
 import Trusted from "./_components/Trusted"
 import Footer from "./_components/Footer"
+import Modul from "./_components/Modul"
+import Cta from "./_components/Cta"
 
 export default function Home () {
     return(
         <div className="">
             <Hero/>
             <Trusted/>
+            <Modul/>
+            <Cta/>
             <Footer/>
         </div>
     )
 }
-=======
-"use client";
 
-import Cta from "./_components/Cta";
-import Modul from "./_components/Modul";
 
-export default function Home() {
-  return (
-    <div className="">
-      <Modul />
-      <Cta />
-    </div>
-  );
-}
->>>>>>> 4808ccf (first)
